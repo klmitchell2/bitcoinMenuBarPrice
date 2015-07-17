6565ebf4c8bd00a065657158ffb8c140dbd3fd58
Kevin Mitchell <temp@temps-MacBook-Pro.local>
2015-07-16 22:22:03 -0500

    Initial Commit

---
+++ bitcoinPrice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bitcoinPrice
+//
+//  Created by Kevin Mitchell on 7/16/15.
+//  Copyright (c) 2015 Kevin Mitchell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ bitcoinPrice/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  bitcoinPrice
+//
+//  Created by Kevin Mitchell on 7/16/15.
+//  Copyright (c) 2015 Kevin Mitchell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
